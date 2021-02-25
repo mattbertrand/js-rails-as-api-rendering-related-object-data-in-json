95cca1cf27be2b676c33780901c7917bfa0f2e5e
github username <github email address>
2021-02-24 16:32:30 -0800

    commit tree c46e015ef9e374d6d3b9f3a944d73ecbc5f69e62

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,16 @@
+class SightingsController < ApplicationController
+    def index
+        sightings = Sighting.all
+        render json: sightings, include: [:bird, :location]
+    end
+    
+    def show
+        sighting = Sighting.find_by(id: params[:id])
+        # render json: { id: sighting.id, bird: sighting.bird, location: sighting.location}
+        if sighting
+            render json: sighting, include: [:bird, :location], except: [:updated_at]
+        else
+            render json: { message: 'No sighting found with that id'}
+        end
+    end
+end
